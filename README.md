4fee5ae1554cd4dda1b4ded6a3fd44cff3f29d5c
Krishna Kumar <kks32@cam.ac.uk>
2017-02-28 15:51:34 +0000

    5R7 Numerical methods

---
+++ README.md
@@ -0,0 +1,3 @@
+# Numerical methods in Geotechnical Engineering
+## LBM-DEM, MPM, LEM, and FEM
+> Krishna Kumar, University of Cambridge. Lent 2017.
---
+++ js/video.js
@@ -0,0 +1,9 @@
+function PlayVideo(videoClipID) {
+   var videoClip = document.getElementById(videoClipID);
+    videoClip.play();
+}
+
+function PauseVideo(videoClipID) {
+   var videoClip = document.getElementById(videoClipID);
+    videoClip.pause();
+}
